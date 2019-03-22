19d2a5ff40c12be980fae50cf15cce3c3d2a74a9
JaylerCD <2732364904@qq.com>
2019-03-22 10:06:03 +0800

    [ADD] 权限检测工具类

---
+++ baselibrary/src/main/java/com/jl/baselibrary/utils/PermissionUtils.java
@@ -0,0 +1,164 @@
+package com.jl.baselibrary.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.PermissionChecker;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 权限检测工具类
+ * 检查权限 --> 请求权限 --> 回调请求结果
+ * Created by JayLer on 2019/3/22.
+ */
+public class PermissionUtils {
+
+    private Context mContext;
+
+    private int mRequestCode;
+
+    private String[] mPermissions;
+
+    private PermissionResultCallback mCallback;
+
+    private static PermissionUtils mPermissionUtils;
+
+    public PermissionUtils(Context context) {
+        this.mContext = context;
+    }
+
+    public static PermissionUtils getInstance() {
+        return mPermissionUtils;
+    }
+
+    public static PermissionUtils with(Context context) {
+        mPermissionUtils = new PermissionUtils(context);
+        return mPermissionUtils;
+    }
+
+    public PermissionUtils permissions(String... permissions) {
+        this.mPermissions = permissions;
+        return mPermissionUtils;
+    }
+
+    public PermissionUtils code(int requestCode) {
+        this.mRequestCode = requestCode;
+        return this;
+    }
+
+
+    /**
+     * 请求权限
+     */
+    public void request(PermissionResultCallback callback) {
+
+        mCallback = callback;
+
+        LinkedList<String> deniedPermissions = getDeniedPermissions(mContext, mPermissions);
+
+        if (deniedPermissions == null || deniedPermissions.isEmpty()) {
+            if (callback != null) {
+                callback.onGranted();
+            }
+            return;
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            String[] requestPermissions = deniedPermissions.toArray(new String[deniedPermissions.size()]);
+            ((Activity) mContext).requestPermissions(requestPermissions, mRequestCode);
+        }
+
+    }
+
+    /**
+     * 检查权限,获取未授权的权限
+     */
+    public LinkedList getDeniedPermissions(Context context, String[] permissions) {
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return null;
+        }
+
+        LinkedList<String> deniedPermissions = new LinkedList<>();
+        for (String permission : permissions) {
+            int checkSelfPermission = checkSelfPermission(context, permission);
+            //如果是拒绝状态则装入拒绝集合中
+            if (checkSelfPermission == PackageManager.PERMISSION_DENIED) {
+                deniedPermissions.add(permission);
+            }
+        }
+
+        return deniedPermissions;
+
+    }
+
+    /**
+     * 检查权限的状态
+     */
+    static int checkSelfPermission(Context context, String permission) {
+        try {
+            final PackageInfo info = context.getPackageManager().getPackageInfo(
+                    context.getPackageName(), 0);
+            int targetSdkVersion = info.applicationInfo.targetSdkVersion;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (targetSdkVersion >= Build.VERSION_CODES.M) {
+                    return ContextCompat.checkSelfPermission(context, permission);
+                } else {
+                    return PermissionChecker.checkSelfPermission(context, permission);
+                }
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return ContextCompat.checkSelfPermission(context, permission);
+    }
+
+
+    /**
+     * 请求权限的回调，在 onRequestPermissionsResult(...) 中使用
+     */
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+
+        if (mCallback == null) {
+            return;
+        }
+
+        if (requestCode != mRequestCode) {
+            return;
+        }
+
+        List<String> permissionList = new ArrayList<>();
+        for (int i = 0; i < grantResults.length; i++) {
+            // 拒绝权限
+            if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
+                permissionList.add(permissions[i]);
+            }
+        }
+
+        if (permissionList.isEmpty()) {
+            mCallback.onGranted();
+        } else {
+            mCallback.onDenied(permissionList);
+        }
+
+    }
+
+
+    public interface PermissionResultCallback {
+        /**
+         * 同意
+         */
+        void onGranted();
+
+        /**
+         * 拒绝
+         */
+        void onDenied(List<String> permissions);
+    }
+}
